e21856069dc202f662f3cb9c73b1decc223c302e
Jane-972 <wafaeboumajjane@gmail.com>
2025-07-15 11:12:41 +0200

    First commit

---
+++ src/main/java/org/jane/gtelinternship/GtelInternshipApplication.java
@@ -0,0 +1,13 @@
+package org.jane.gtelinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtelInternshipApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GtelInternshipApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/jane/gtelinternship/GtelInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jane.gtelinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtelInternshipApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
